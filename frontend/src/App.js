c828d1dbd33f61fbf6a4f2fdd76e37b19e9ebf52
PraveenBimsara <praveenbimsara99@gmail.com>
2025-04-01 20:47:32 +0530

    get todos from the db

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
+import { useEffect, useState } from "react";
 import ToDo from "./components/ToDo";
+import { getAllToDo } from "./utils/HandleApi";
 
 function App() {
+  const [toDo, setToDo] = useState([]);
+
+  useEffect(() => {
+    getAllToDo(setToDo);
+  }, []);
+
   return (
     <div className="App">
-        <div className="container">
-          <h1>TODO App</h1>
-          <div className="top">
-            <input type="text" placeholder="Add Todos..."/>
-            
-            <div className="add">Add</div>
-          </div>
-          <div className="list">
-            <ToDo text="Hiii"/>
-            <ToDo text="Hiii"/>
-            <ToDo text="Hiii"/>
-            <ToDo text="Hiii"/>
-          </div>
+      <div className="container">
+        <h1>TODO App</h1>
+        <div className="top">
+          <input type="text" placeholder="Add Todos..." />
+
+          <div className="add">Add</div>
         </div>
+        <div className="list">
+          {toDo.map((item) => (
+            <ToDo key={item._id} text={item.text} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
